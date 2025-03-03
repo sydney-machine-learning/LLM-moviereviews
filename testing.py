bb1e347d1244328f310cfefa725f9a0aa9a320b7
bts1971 <brendan.t.sands@gmail.com>
2025-03-03 18:42:32 +1100

    split script into 2 scripts

--- testing.py
+++ testing.py
@@ -6,53 +6,53 @@ from collections import Counter
 
 
 # Dictionary to store imdb reviews grouped by rating
-imdb_reviews_dict = {}
+#imdb_reviews_dict = {}
 
 # list of unique imdb ids
-Unique_IMDB_ids = set()  
+#Unique_IMDB_ids = set()  
 
 # List to store all DataFrames of reviews
-all_reviews = []
+#all_reviews = []
 
 # Download from Kaggle
 path_subtitles = kagglehub.dataset_download("mlopssss/subtitles")
-path_imdb_reviews = kagglehub.dataset_download("mlopssss/imdb-movie-reviews-grouped-by-ratings")
+#path_imdb_reviews = kagglehub.dataset_download("mlopssss/imdb-movie-reviews-grouped-by-ratings")
 
 common_exclusions = {'-','♪','i', 'you', 'to', 'the', 'a', 'and', 'it', 'is', 'that', 'of','s', 't', 'what', 'in', 'me', 'this', 'on', 'sir', 'get','for', 'she', 'be', 'eve', 'not', 'have', 'all', 'her', 'was', 'my','can', 'oh', 'no', 'we', 'well', 'annie', 'be', 'he', 'like', 'don'}
 
-def custom_csv_reader(file_path):
-    data = []
-    with open(file_path, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
-        current_id = None
-        current_review_number = None
-        current_review = []
-        for line in lines[1:]:  # Skip the first line (header)
-            if line.startswith('tt'):
-                if current_id is not None:
-                    data.append([current_id, current_review_number, ' '.join(current_review)])
-                parts = line.split(',', 2)  # Split only on the first two commas
-                current_id = parts[0]
-                current_review_number = parts[1]
-                current_review = [parts[2].strip()]
-            else:
-                current_review.append(line.strip())
-        if current_id is not None:
-            data.append([current_id, current_review_number, ' '.join(current_review)])
-    return pd.DataFrame(data, columns=['MovieID', 'ReviewNumber', 'Review'])
+#def custom_csv_reader(file_path):
+#    data = []
+#    with open(file_path, 'r', encoding='utf-8') as file:
+#        lines = file.readlines()
+#        current_id = None
+#        current_review_number = None
+#        current_review = []
+#        for line in lines[1:]:  # Skip the first line (header)
+#            if line.startswith('tt'):
+#                if current_id is not None:
+#                    data.append([current_id, current_review_number, ' '.join(current_review)])
+#                parts = line.split(',', 2)  # Split only on the first two commas
+#                current_id = parts[0]
+#                current_review_number = parts[1]
+#                current_review = [parts[2].strip()]
+#            else:
+#                current_review.append(line.strip())
+#        if current_id is not None:
+#            data.append([current_id, current_review_number, ' '.join(current_review)])
+#    return pd.DataFrame(data, columns=['MovieID', 'ReviewNumber', 'Review'])
 
-for i in range(1, 11):
+#for i in range(1, 11):
     #df = pd.read_csv(f"{path_imdb_reviews}/reviews_rating_{i}.csv")
-    df = custom_csv_reader(f"{path_imdb_reviews}/reviews_rating_{i}.csv")
-    imdb_reviews_dict[i] = df
-    Unique_IMDB_ids.update(df["MovieID"].tolist())
-    all_reviews.append(df)
+#    df = custom_csv_reader(f"{path_imdb_reviews}/reviews_rating_{i}.csv")
+#    imdb_reviews_dict[i] = df
+#    Unique_IMDB_ids.update(df["MovieID"].tolist())
+#    all_reviews.append(df)
 
 # Combine all reviews into a single DataFrame and save to csv
-all_reviews_df = pd.concat(all_reviews, ignore_index=True)
-all_reviews_df.to_csv('all_imdb_reviews.csv', index=False, header=False)
+#all_reviews_df = pd.concat(all_reviews, ignore_index=True)
+#all_reviews_df.to_csv('all_imdb_reviews.csv', index=False, header=False)
 
-print(all_reviews_df.head())
+#print(all_reviews_df.head())
 
 def parse_srt_excluding_common(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -116,6 +116,10 @@ for group in ['Oscar', 'Blockbusters']:
                         'bodyContent': content
                     })
     df = pd.DataFrame(data)
+    
+    print(df[['movie', 'year','bodyContent']].columns)
+
+    print(df[['movie', 'year','bodyContent']].head)
 
     # Save Blockbuster movie titles and years to a CSV file
-    df[['movie', 'year']].to_csv(f'{group.lower()}_movies_and_years.csv', index=False, header=False)
+    df[['movie', 'year','bodyContent']].to_csv(f'{group.lower()}_movies_and_years.csv', index=False, header=False)
